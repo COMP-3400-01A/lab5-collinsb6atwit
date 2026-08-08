b28aa8975917e005fb1868e50502b88028d8d75c
Brendan Collins <collinsb6@wit.edu>
2026-08-07 21:55:06 -0400

    Completed Parts B and C

--- partb.c
+++ partb.c
@@ -7,5 +7,41 @@
 
 int main(int argc, char* argv[]) {
     // TODO: Complete and document
+
+    if (argc != 3) {
+        printf("USAGE: partb FILEIN FILEOUT\n");
+        return 1;
+    }//end if
+
+    char* input_filename = argv[1];
+    char* output_filename = argv[2];
+
+    int fd = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, 0770);
+
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }//end if
+
+    if (dup2(fd, STDOUT_FILENO) == -1) {
+        perror("dup2");
+        close(fd);
+        return 1;
+    }//end if
+
+    close(fd);
+
+    printf("Category,Count\n");
+    fflush(stdout);
+
+    char* parta_args[] = { "./parta", input_filename, NULL };
+
+    int eret = execv("./parta", parta_args);
+
+    if (eret == -1) {
+        perror("execv");
+        return 1;
+    }//end if
+
     return 0;
 }
--- partc.c
+++ partc.c
@@ -8,5 +8,61 @@
 int main(int argc, char* argv[]) {
     // TODO: Complete and document
 
+    if (argc != 2) {
+        printf("USAGE: partc FILE\n");
+        return 1;
+    }//end if
+
+    int pipe_fd[2];
+
+    if (pipe(pipe_fd) == -1) {
+        perror("pipe");
+        return 1;
+    }//end if
+
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("fork");
+        return 1;
+    }//end if
+
+    if (pid > 0) {
+        close(pipe_fd[0]);
+
+        if (dup2(pipe_fd[1], STDOUT_FILENO) == -1) {
+            perror("dup2");
+            return 1;
+        }//end if
+
+        close(pipe_fd[1]);
+
+        char* parta_args[] = { "./parta", argv[1], NULL };
+
+        execv("./parta", parta_args);
+
+        perror("execv");
+
+        return 1;
+    }//end if
+
+    else {
+        close(pipe_fd[1]);
+
+        if (dup2(pipe_fd[0], STDIN_FILENO) == -1) {
+            perror("dup2");
+            return 1;
+        }//end if
+
+        close(pipe_fd[0]);
+
+        char* sort_args[] = { "sort", "-t,", "-k2", "-n", NULL };
+
+        execvp("sort", sort_args);
+
+        perror("execvp");
+        return 1;
+    }//end else
+
     return 0;
 }
